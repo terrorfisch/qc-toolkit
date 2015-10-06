91cc87219718ad3b55dee79aa7c320af793205ab
terryFitch <humsim@t-online.de>
2015-10-06 15:56:34 +0200

    Testing test for TRAVIS

--- tests/pulses/BranchPulseTemplateTests.py
+++ tests/pulses/BranchPulseTemplateTests.py
@@ -1,11 +1,6 @@
 import unittest
-import os
-import sys
-from src.pulses.BranchPulseTemplate import BranchPulseTemplate
+from qctoolkit.pulses import BranchPulseTemplate
 
-
-srcPath = os.path.dirname(os.path.abspath(__file__)).rsplit('tests',1)[0] + 'src'
-sys.path.insert(0,srcPath)
 
 class GenericBranchPulseTemplateTest(unittest.TestCase):
     pass
