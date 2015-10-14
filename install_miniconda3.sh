38bf96ba4e6cf57d6c4771ed1ca2b74db484b00d
terrorfisch <terrorfisch@mailbox.org>
2015-10-14 11:33:25 +0200

    next try

--- install_miniconda3.sh
+++ install_miniconda3.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ ! -d $HOME/.cache/clean_miniconda3/bin ]; then
+if [ ! -d $HOME/.cache/clean_miniconda3/$PYTHON_VERSION ]; then
     wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/Miniconda3-latest-Linux-x86_64.sh
     chmod +x $HOME/Miniconda3-latest-Linux-x86_64.sh
     $HOME/Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
@@ -13,9 +13,9 @@ if [ ! -d $HOME/.cache/clean_miniconda3/bin ]; then
     
     $HOME/miniconda3/bin/conda clean --yes --tarballs
     
-    cp -r $HOME/miniconda3 $HOME/.cache/clean_miniconda3
+    cp -r $HOME/miniconda3 $HOME/.cache/clean_miniconda3/$PYTHON_VERSION
 else
     echo "Use chached miniconda enviroment."
-    cp -r $HOME/.cache/clean_miniconda3 $HOME/miniconda3
+    cp -r $HOME/.cache/clean_miniconda3/$PYTHON_VERSION $HOME/miniconda3
 fi
 
